bdebf7959e5cdc4a75adbde6637d91bc8fb8c0db
marcingal <marcin921125@gmail.com>
2018-11-02 12:00:19 +0100

    button made with render function

--- ClickableButton.js
+++ ClickableButton.js
@@ -1,11 +1,13 @@
 function ClickableButton(label, onClickHandler) {
-    const toggleButton = document.createElement('button');
-    toggleButton.style.height = '60px';
-    toggleButton.style.width = '200px';
-    toggleButton.style.position = 'fixed';
-    toggleButton.style.left = '40vw';
+    this.label = label;
+    this.onClickHandler = onClickHandler;
 
-    toggleButton.innerHTML = `<p>${label}</p>`;
-    toggleButton.addEventListener('click', onClickHandler);
-    document.body.appendChild(toggleButton);
+    this.render()
+}
+
+ClickableButton.prototype.render = function () {
+    const button = document.createElement('button');
+    button.innerText = this.label;
+    button.addEventListener('click', this.onClickHandler);
+    document.body.appendChild(button);
 }
