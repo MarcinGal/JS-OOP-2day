c72bd71a7a7bcef96333fcc892f9eabd1ccad850
marcingal <marcin921125@gmail.com>
2018-11-02 11:06:04 +0100

    added function toggle to menu

--- script1.js
+++ script1.js
@@ -1,25 +1,48 @@
-function SidebarMenu() {
+function SidebarMenu(color, direction) {
     this.isOpen = true;
-    this.isOnTheLeft = true;
-    this.isOnTheRight = false;
-    this.bgColor = 'orangered';
+    this.isOnTheLeft = direction === 'left' ? true : false;
+    this.isOnTheRight = direction === 'right' ? true : false;
+    if (!this.isOnTheLeft && !this.isOnTheRight) {
+        this.isOnTheLeft = true;
+    }
+    this.bgColor = color || 'orangered';
+    this.init();
+}
+
+SidebarMenu.prototype.init = function () {
+    this.render()
 }
 
 SidebarMenu.prototype.render = function () {
-    const menuDiv = document.createElement('div');
+    if (this.menuDiv) this.menuDiv.remove();
+    this.menuDiv = document.createElement('div');
 
-    menuDiv.style.backgroundColor = this.bgColor;
-    menuDiv.style.position = 'fixed';
-    menuDiv.style.top = '0';
-    menuDiv.style.width = '200px';
-    menuDiv.style.height = '100vh';
+    this.menuDiv.style.backgroundColor = this.bgColor;
+    this.menuDiv.style.position = 'fixed';
+    this.menuDiv.style.top = '0';
+    this.menuDiv.style.width = '200px';
+    this.menuDiv.style.height = '100vh';
 
-    if (this.isOnTheLeft) menuDiv.style.left = '0';
-    if (this.isOnTheRight) menuDiv.style.right = '0';
-    if (!this.isOpen) menuDiv.style.display = 'none';
+    if (this.isOnTheLeft) this.menuDiv.style.left = '0';
+    if (this.isOnTheRight) this.menuDiv.style.right = '0';
+    if (!this.isOpen) this.menuDiv.style.display = 'none';
 
-    document.body.appendChild(menuDiv);
+    document.body.appendChild(this.menuDiv);
+
 }
 
+SidebarMenu.prototype.toggleMenu = function () {
+    if (this.isOpen) {
+        this.isOpen = false;
+    }
+    else {
+        this.isOpen = true;
+    }
+    this.render();
+}
+
+
 const menu1 = new SidebarMenu();
-menu1.render();
+const menu2 = new SidebarMenu('green', 'right');
+menu2.toggleMenu()
+menu2.toggleMenu()
